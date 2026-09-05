f705fc6e9f8bafcdb682a01160259e0294a4e939
ganesh <gkumar.krishnasamy@gmail.com>
2026-09-05 14:47:15 +0530

    Initial commit: sample app + Dockerfile

---
+++ app.js
@@ -0,0 +1,14 @@
+// app.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const VERSION = '1.0';
+app.get('/', (req, res) => {
+ res.json({
+ message: 'Hello from the CI/CD pipeline!',
+ version: VERSION
+ });
+});
+app.listen(PORT, () => {
+ console.log(`App running on port ${PORT}, version ${VERSION}`);
+});
